27586ef741cecdd4c6ce9ce5a680a67659b14ab6
Andrey <sytnik.andrey.ua@gmail.com>
2019-07-10 21:18:23 +0300

    Updates

---
+++ precache-manifest.aa9cc5b1983fb581c9f4bd3cd6b5c2cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2c640234bc0a0be585037c8b447121",
+    "url": "/react-pizza-app/index.html"
+  },
+  {
+    "revision": "5c3f294300acca4493aa",
+    "url": "/react-pizza-app/static/css/main.873d95d3.chunk.css"
+  },
+  {
+    "revision": "b60f03b7109d9a8a0253",
+    "url": "/react-pizza-app/static/js/2.dcf2a7db.chunk.js"
+  },
+  {
+    "revision": "5c3f294300acca4493aa",
+    "url": "/react-pizza-app/static/js/main.7d6fc28d.chunk.js"
+  },
+  {
+    "revision": "b22c7be2ef430ecbe054",
+    "url": "/react-pizza-app/static/js/runtime~main.cfe1ee2b.js"
+  },
+  {
+    "revision": "18508e6012804bed65b8c2cf61af780a",
+    "url": "/react-pizza-app/static/media/ingridients.18508e60.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pizza-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "57c1c517f1b497af14e0e5cfc75569ec",
+    "url": "/react-pizza-app/static/media/pizza_bg.57c1c517.jpg"
+  },
+  {
+    "revision": "965d29b26efb5134698d0da5f1f6c702",
+    "url": "/react-pizza-app/static/media/pizza_preview.965d29b2.jpg"
+  }
+]);
